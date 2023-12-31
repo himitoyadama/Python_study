dc9771a846dd2cf44a809de413fd9aed0828b94a
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2023-12-31 18:33:28 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const foodRouter = require("./routes/foodRoutes");
+
+app.use(foodRouter);
+/*データベース接続*/ 
+mongoose
+.connect("mongodb+srv://vampire6cult:hMnlKUxSIe0AVHbY@cluster0.3z7xw0b.mongodb.net/food?retryWrites=true&w=majority")
+.then(() => console.log("データベース接続に成功しました"))
+.catch((err) => console.log(err))
+
+app.listen(3003, () => {
+  console.log("サーバーが起動しました");
+});
